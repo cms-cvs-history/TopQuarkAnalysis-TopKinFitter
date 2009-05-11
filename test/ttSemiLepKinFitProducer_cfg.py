7741e2d05258c90ed7c821aba70752687bb21eb6
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-05-11 12:58:21 +0000

    include OutputModule and outpath; use test file on afs as default input again

--- test/ttSemiLepKinFitProducer_cfg.py
+++ test/ttSemiLepKinFitProducer_cfg.py
@@ -16,14 +16,17 @@ process.MessageLogger.cerr.INFO = cms.untracked.PSet(
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
+    ## small test file on afs
+    'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_2_X_2008-11-03-STARTUP_V7-AODSIM.100.root'
     ## relval sample with 9,000 events
-    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/18B597E9-BB0B-DE11-8232-0030487A322E.root',
-    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/462BE681-490C-DE11-8558-000423D98750.root',
-    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/8646BC5A-C20B-DE11-96D8-001617DBD316.root',
-    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/BAA26726-C00B-DE11-858D-001D09F24EE3.root',
-    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/D4D3FE46-BD0B-DE11-8F6C-000423D9880C.root',
-    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/F802372C-BB0B-DE11-B812-000423D99AA2.root'
-    )
+    #'/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/18B597E9-BB0B-DE11-8232-0030487A322E.root',
+    #'/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/462BE681-490C-DE11-8558-000423D98750.root',
+    #'/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/8646BC5A-C20B-DE11-96D8-001617DBD316.root',
+    #'/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/BAA26726-C00B-DE11-858D-001D09F24EE3.root',
+    #'/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/D4D3FE46-BD0B-DE11-8F6C-000423D9880C.root',
+    #'/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/F802372C-BB0B-DE11-B812-000423D99AA2.root'
+    ),
+     skipEvents = cms.untracked.uint32(0)
 )
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
@@ -54,7 +57,10 @@ process.p = cms.Path(process.patDefaultSequence *
 ## configure output module
 process.out = cms.OutputModule("PoolOutputModule",
     SelectEvents   = cms.untracked.PSet(SelectEvents = cms.vstring('p') ),                               
-    fileName = cms.untracked.string('ttSemiLepKinFitProducer.root')
+    fileName = cms.untracked.string('ttSemiLepKinFitProducer.root'),
+    outputCommands = cms.untracked.vstring('drop *')
 )
+process.out.outputCommands += ['keep *_kinFitTtSemiLepEvent_*_*']
+
 ## output path
-#process.outpath = cms.EndPath(process.out)
+process.outpath = cms.EndPath(process.out)
