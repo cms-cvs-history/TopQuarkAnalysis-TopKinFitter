c14ceb1c4c72790383d48915d05db8d831d2f503
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-05-11 09:42:59 +0000

    adapt to PATv2

--- test/ttSemiLepKinFitProducer_cfg.py
+++ test/ttSemiLepKinFitProducer_cfg.py
@@ -1,8 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#-------------------------------------------------
-# test cfg file for the ttSemiLepKinFitProducer
-#-------------------------------------------------
 process = cms.Process("TEST")
 
 ## add message logger
@@ -16,53 +13,48 @@ process.MessageLogger.cerr.INFO = cms.untracked.PSet(
     KinFitter           = cms.untracked.PSet( limit = cms.untracked.int32(-1) )
 )
 
-#-------------------------------------------------
-# process configuration
-#-------------------------------------------------
-
-
 ## define input
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root')
+    fileNames = cms.untracked.vstring(
+    ## relval sample with 9,000 events
+    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/18B597E9-BB0B-DE11-8232-0030487A322E.root',
+    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/462BE681-490C-DE11-8558-000423D98750.root',
+    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/8646BC5A-C20B-DE11-96D8-001617DBD316.root',
+    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/BAA26726-C00B-DE11-858D-001D09F24EE3.root',
+    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/D4D3FE46-BD0B-DE11-8F6C-000423D9880C.root',
+    '/store/relval/CMSSW_2_2_6/RelValTTbar/GEN-SIM-RECO/IDEAL_V12_v1/0002/F802372C-BB0B-DE11-B812-000423D99AA2.root'
+    )
 )
-
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
 )
-
 ## configure process options
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(False)
 )
 
-## configure geometry
+## configure geometry & conditions
 process.load("Configuration.StandardSequences.Geometry_cff")
-
-## configure conditions
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('STARTUP_V4::All')
-
-## load magnetic field
 process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('IDEAL_V12::All')
 
-#-------------------------------------------------
-# tqaf configuration
-#-------------------------------------------------
-
-## std sequence for tqaf layer1
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_cff")
-
-## necessary fixes to run 2.2.X on 2.1.X data
-## comment this when running on samples produced
-## with 22X
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
-run22XonSummer08AODSIM(process)
+## std sequence for pat
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
 ## std sequence to produce the kinematic fit for semi-leptonic events
 process.load("TopQuarkAnalysis.TopKinFitter.TtSemiLepKinFitProducer_Muons_cfi")
 
 ## process path
-process.p = cms.Path(process.tqafLayer1  *
+process.p = cms.Path(process.patDefaultSequence *
                      process.kinFitTtSemiLepEvent
                      )
+
+## configure output module
+process.out = cms.OutputModule("PoolOutputModule",
+    SelectEvents   = cms.untracked.PSet(SelectEvents = cms.vstring('p') ),                               
+    fileName = cms.untracked.string('ttSemiLepKinFitProducer.root')
+)
+## output path
+#process.outpath = cms.EndPath(process.out)
